1eb115dd15c58558bca616f92e115882ca76bbb4
SumedhYetam <usomeone48@gmail.com>
2024-06-28 17:25:06 +0530

    using function

--- profitCalculator.go
+++ profitCalculator.go
@@ -8,17 +8,37 @@ func main() {
 	var expenses float64
 	const tax_rate float64 = 30
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	// fmt.Print("Enter revenue: ")
+	// fmt.Scan(&revenue)
+	revenue = getUserInput("Enter revenue: ")
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	// fmt.Print("Enter expenses: ")
+	// fmt.Scan(&expenses)
+	expenses = getUserInput("Enter expenses: ")
 
+	// profit_before_tax := revenue - expenses
+	// fmt.Println("profit_before_tax: ", profit_before_tax)
+
+	// profit_after_tax := profit_before_tax * (1 - (tax_rate / 100))
+	// fmt.Println("profit_after_tax: ", profit_after_tax)
+
+	// fmt.Println("ratio: ", profit_before_tax/profit_after_tax)
+	pbt, pat, ratio := calculate(revenue, expenses, tax_rate)
+	fmt.Println("profit_before_tax: ", pbt)
+	fmt.Println("profit_after_tax: ", pat)
+	fmt.Println("ratio: ", ratio)
+}
+
+func getUserInput(Text string) float64 {
+	var userInput float64
+	fmt.Println(Text)
+	fmt.Scan(&userInput)
+	return userInput
+}
+
+func calculate(revenue float64, expenses float64, tax_rate float64) (float64, float64, float64) {
 	profit_before_tax := revenue - expenses
-	fmt.Println("profit_before_tax: ", profit_before_tax)
-
 	profit_after_tax := profit_before_tax * (1 - (tax_rate / 100))
-	fmt.Println("profit_after_tax: ", profit_after_tax)
-
-	fmt.Println("ratio: ", profit_before_tax/profit_after_tax)
+	ratio := profit_before_tax / profit_after_tax
+	return profit_before_tax, profit_after_tax, ratio
 }
